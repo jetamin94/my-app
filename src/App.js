cec38c919f101af898432d981eff8fe20d62423f
ThangNV <jetamin94@gmail.com>
2018-03-28 11:00:06 +0700

    update cmc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import ToDoList from './components/ToDoList';
+import MarketCap from './components/MarketCap';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ToDoList />
+        <MarketCap />
       </div>
     );
   }
---
+++ src/components/MarketCap.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+export default class MarketCap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+
+  fetchFirst(url) {
+    var that = this;
+    if (url) {
+      fetch(url).then(response => response.json())
+        .then(function (result) {
+          that.setState({
+            list: result
+          });
+        });
+    }
+  }
+  componentWillMount() {
+    this.fetchFirst('https://api.coinmarketcap.com/v1/ticker/?limit=10');
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>CMC</h3>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Symbol</th>
+              <th>Price USD</th>
+              <th>Price BTC</th>
+              <th>percent_change_1h</th>
+              <th>percent_change_24h</th>
+              <th>percent_change_7d</th>
+            </tr>
+            {this.state.list.map(post =>
+              <tr key={post.id}>
+                <td>{post.name}</td>
+                <td>{post.symbol}</td>
+                <td>{post.price_usd}</td>
+                <td>{post.price_btc}</td>
+                <td>{post.percent_change_1h}</td>
+                <td>{post.percent_change_24h}</td>
+                <td>{post.percent_change_7d}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
